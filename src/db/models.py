70c52ae956bb38772e58645240de3a53d3764401
Mohamed Rirash <codesavvylabs@gmail.com>
2025-05-13 06:51:28 +0300

    book schema and models is created

---
+++ src/db/models.py
@@ -0,0 +1,22 @@
+import uuid
+from datetime import date, datetime
+from typing import Optional
+
+import sqlalchemy.dialects.postgresql as pg
+from sqlmodel import Column, Field, SQLModel
+
+
+class Book(SQLModel, table=True):
+    __tablename__ = "books"  # type: ignore
+    uid: uuid.UUID = Field(
+        sa_column=Column(pg.UUID, nullable=False, primary_key=True, default=uuid.uuid4)
+    )
+    title: str
+    author: str
+    publisher: str
+    published_date: date
+    page_count: int
+    language: str
+    user_uid: Optional[uuid.UUID] = Field(default=None, foreign_key="users.uid")
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    update_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
